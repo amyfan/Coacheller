c223baea7f735de25f9646d2ff6bc51ed41a6b71
Michael Alex <malex.sd@gmail.com>
2012-04-05 14:38:31 -0700

    Issue with CoachellerServlet.java resolved; full refresh of all projects in package explorer was necessary.

--- Coacheller-AppEngine/src/com/coacheller/server/service/CoachellerServlet.java
+++ Coacheller-AppEngine/src/com/coacheller/server/service/CoachellerServlet.java
@@ -1,10 +1,6 @@
 package com.coacheller.server.service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -13,21 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 
-import com.coacheller.server.domain.DayEnum;
 import com.coacheller.server.domain.Set;
 import com.coacheller.server.logic.JSONUtils;
 import com.coacheller.server.logic.RatingManager;
-import com.coacheller.server.logic.SetDataLoader;
+import com.coacheller.shared.DayEnum;
 import com.coacheller.shared.FieldVerifier;
-
 
 /**
  * HTTP Servlet, intended for Android/etc. device use.
- *
+ * 
  * @author Amy
- *
+ * 
  */
-
 @SuppressWarnings("serial")
 public class CoachellerServlet extends HttpServlet {
 
@@ -74,22 +67,6 @@ public class CoachellerServlet extends HttpServlet {
 
     if (action.compareToIgnoreCase("load") == 0) {
 
-  /**
-   *
-   * @param url
-   */
-  private void loadFile(String url) {
-    try {
-      URL inputData = new URL(url);
-      URLConnection urlConn = inputData.openConnection();
-      InputStreamReader is = new InputStreamReader(urlConn.getInputStream(), "UTF8");
-      BufferedReader in = new BufferedReader(is);
-      SetDataLoader dataLoader = SetDataLoader.getInstance();
-      RatingManager ratingMgr = RatingManager.getInstance();
-      ratingMgr.deleteAllSets();
-      dataLoader.insertSets(in);
-    } catch (Exception e) {
-    } finally {
     }
 
   }
